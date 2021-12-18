6a9c23735929d0694b5cbddefdd56c1c53eb9c05
l00pss <mammadli.vugar@hotmail.com>
2021-12-18 11:07:45 -0800

    .

--- src/az/rock/util/BoostArray.java
+++ src/az/rock/util/BoostArray.java
@@ -1,27 +1,37 @@
 package az.rock.util;
 
-import java.util.AbstractList;
-import java.util.List;
-import java.util.RandomAccess;
+import java.util.*;
 
 public class BoostArray<E> extends AbstractList<E>
         implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
     @java.io.Serial
-    private static final long serialVersionUID = 8683452581122892189L;
+    private static final long serialVersionUID = 8683452501122892189L;
 
     /**
      * Default initial capacity.
      */
-    private static final int DEFAULT_CAPACITY = 10;
+    private final int DEFAULT_CAPACITY = 10;
+
+    private final float INCREMENT_CAPACITY_RANGE = 1.1F;
+
+    private final int NULL_SAFETY_CAPACITY = DEFAULT_CAPACITY / 2;
+
+    private E[] boostArray;
+
+    private final int[] nullSafety = new int[NULL_SAFETY_CAPACITY];
+
+    public BoostArray() {
+        boostArray = (E[]) new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public E get(int index) {
-        return null;
+        return boostArray[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return boostArray.length;
     }
 
     @Override
@@ -35,5 +45,9 @@ public class BoostArray<E> extends AbstractList<E>
         }
     }
 
+    @Override
+    public boolean add(E e) {
 
+        return super.add(e);
+    }
 }
