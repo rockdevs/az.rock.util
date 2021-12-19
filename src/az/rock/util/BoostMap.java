753a5e733841d05ded5e9c32b7c82e51daedcdc2
l00pss <mammadli.vugar@hotmail.com>
2021-12-19 07:35:48 -0800

    .

--- src/az/rock/util/BoostMap.java
+++ src/az/rock/util/BoostMap.java
@@ -1,17 +1,66 @@
 package az.rock.util;
 
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class BoostMap<K,V> extends AbstractMap<K,V> implements Map<K,V> {
-    Node<K,V>[] map;
+public final class BoostMap<K,V> extends AbstractMap<K,V> implements Map<K,V> {
 
-    private record Node<K,V>(K key,V value){};
+    private final int initialCapacity  = 1000;
+
+    private int lastFreeIndex = 0;
+
+    //Keyleri burada tutacam
+    private final Set<KeyNode<K>> keySet = new HashSet<>();
 
 
+
+    //Deyerleri burada tutacam
+    @SuppressWarnings("unchecked")
+    private final V[] values = (V[]) new Object[initialCapacity];
+
+    private class KeyNode<K>{
+        int index;
+        K key;
+
+        public KeyNode(K key) {
+            this.key = key;
+            index = lastFreeIndex;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            KeyNode<?> keyNode = (KeyNode<?>) o;
+            return Objects.equals(key, keyNode.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+    }
+
+
+    @Override
+    public V put(K key, V value) {
+        if(keySet.add(new KeyNode<>(key))){
+            values[lastFreeIndex] = value;
+            lastFreeIndex++;
+            return null;
+        }else {
+            return value;
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        for (KeyNode<K> keyNode : keySet) {
+            if (keyNode.key == key)
+                return values[keyNode.index];
+        }
+        return null;
+    }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
@@ -19,4 +68,11 @@ public class BoostMap<K,V> extends AbstractMap<K,V> implements Map<K,V> {
     }
 
 
+    @Override
+    public String toString() {
+        return "BoostMap{" +
+                "keySet=" + keySet +
+                ", values=" + Arrays.toString(values) +
+                "}\n";
+    }
 }
